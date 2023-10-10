a2509896a3e8c41c183f8c1dc541db34372cebef
maxlachoucroute <maxime.huds@gmail.com>
2023-10-10 11:39:00 +0200

    Debut du site

---
+++ index-musee.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Musee Mauroide</title>
+  <link rel="stylesheet" href="style-index-musee.css">
+
+</head>
+<body>
+
+<h1>Formulaire de connexion</h1>
+  <div class="container">
+    <div class="cta-form">
+      <h2>Connexion</h2> 
+    <p>Le login pour se connecter au portail d'administration est "admin" et le mot de passe : ""</p>
+    </div>
+    <form action="" class="form">
+      
+      <input type="text" placeholder="Name" class="form__input" id="name" />
+      <label for="name" class="form__label">Login</label>
+
+      <input type="email" placeholder="Email" class="form__input" id="email" />
+      <label for="email" class="form__label">Mot de passe</label>
+
+      
+    </form>
+  </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Le journal</title>
+</head>
+<body>
+
+    <h1>AAAAAAA</h1>
+
+
+<!-- MOT DE PASSE : -->
+
+<a href="index-musee.php">musee-mauroide officiel</a>
+</body>
+
+
+
+
+</html>
